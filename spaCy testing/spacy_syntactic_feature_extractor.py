8e348bb93c088101e56a7c17c3470cea081aa93a
AgustiGM <53912168+AgustiGM@users.noreply.github.com>
2022-05-19 11:46:01 +0200

    Generalized the script to accept different inputs.

--- spaCy testing/spacy_syntactic_feature_extractor.py
+++ spaCy testing/spacy_syntactic_feature_extractor.py
@@ -1,4 +1,6 @@
+import argparse
 import json
+import sys
 
 import spacy
 from spacy import Language
@@ -42,12 +44,23 @@ def set_boundaries(doc):
 nlp = spacy.load("en_core_web_trf")
 nlp.add_pipe('set_boundaries', before='parser')
 
+argument_parser = argparse.ArgumentParser('Program to extract app features from natural language text.')
+argument_parser.add_argument('--text', dest='text_file', type=str, nargs='?',help='File with the text to process. Should '
+                                                                               'be in plain txt')
+argument_parser.add_argument('--expected', dest='expected_results', nargs='?', type=str, help='File with the expected '
+                                                                                           'results. For now, a json')
+
+arguments = argument_parser.parse_args()
+# if len(sys.argv) < 3:
+#     argument_parser.print_help()
+#     sys.exit(2)
+
 # loading text to extract features from
-with open('osmand-alt.txt', 'r', encoding='utf-8') as alt_osmand:
+with open(arguments.text_file, 'r', encoding='utf-8') as alt_osmand:
     text = alt_osmand.read()
 
 # load features to compare
-with open('OsmAnd-features-Description(AlternativeTo).json', 'r', encoding='utf-8') as file:
+with open(arguments.expected_results, 'r', encoding='utf-8') as file:
     raw_expected_results = json.load(file)
 
 expected_results = [x.lower() for x in raw_expected_results]
@@ -63,7 +76,6 @@ for i in range(3):
     alt_chunks = []
     for chunk in alt_document.noun_chunks:
         if chunk.root.dep_ in syntactic_dependencies:
-            # alt_chunks.append((chunk.root.head.text, chunk.text))
             alt_chunks.append(f'{chunk.root.head.text.lower()} {chunk.text.lower()}')
     chunks[i] = alt_chunks
     with open('osmand_alt_automatic_features.txt', 'a', encoding='utf-8') as file:
@@ -81,11 +93,10 @@ for i in range(3):
         for expected in expected_results:
             if expected in chunk or chunk in expected:
                 true_positives.append(chunk)
-    # true_positives = [x for x in chunks[i] if x in [y for y in expected_results if y in x]]
     precision = round(len(true_positives) / len(chunks[i]), 3)
     recall = round(len(true_positives) / len(expected_results), 3)
-    fscore = round(2 * (precision * recall) / (precision + recall), 3)
-    metrics.append((precision, recall, fscore))
+    f_score = round(2 * (precision * recall) / (precision + recall), 3)
+    metrics.append((precision, recall, f_score))
     for j in range(3):
         if i != j:
             inters = [x for x in chunks[i] if x in chunks[j]]
@@ -94,8 +105,8 @@ for i in range(3):
             if diff:
                 differences[f'{i} minus {j}'] = diff
 
-with open('metrics.txt', 'w', encoding='utf-8') as file:
-    print(f'precision\trecall\tfscore', file=file)
+with open(f'metrics-for{arguments.expected_results}.txt', 'w', encoding='utf-8') as file:
+    print(f'precision\trecall\tf-score', file=file)
     for metric in metrics:
         print(f'{metric[0]}\t{metric[1]}\t{metric[2]}', file=file)
 
